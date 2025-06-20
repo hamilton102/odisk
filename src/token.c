f63f8eb185a79ad1dd759abc8beefbfddc7680b6
hamilton102 <hamiltonaferris@gmail.com>
2025-06-20 14:27:17 -0500

    wip: token parsing

--- src/token.c
+++ src/token.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "token.h"
+
+#define MAX_HEADER_LEN 256
+#define MAX_LINE_LEN 256
 
 enum Token {
   /* headers */
-  LEFT_BRACKET, RIGHT_BRACKET,
+  LEFT_BRACKET = '[', RIGHT_BRACKET = ']',
 
   /* main config tokens */
-  EQUAL, SEMICOLON, QUOTE, EOL
+  EQUAL = '=', SEMICOLON = ';', QUOTE = '"'
 };
 
 struct ini {
@@ -19,20 +23,27 @@ struct ini {
 
 struct ini* parse_line(char* input) {
   struct ini* pair;
-  int count = 0;
+  int count, buf_count = 0;
   char current = input[count];
+  char header_buf[MAX_HEADER_LEN];
+  char line_buf[MAX_LINE_LEN];
   while(current != '\n' && current != '\0') {
     /* this is where we'll actually parse the tokens out */
     switch(current) {
       case LEFT_BRACKET:
-        /* do stuff until the right bracket */
-        while (current != ']') {
-          if (current == '\n' || current == '\0') {
+        /* increment the left bracket */
+        ++count;
+        while (current != RIGHT_BRACKET) {
+          if (current == '\n'|| current == '\0') {
             error("Unclosed character [!");
           }
           /* assume until right bracket that it all goes to one header */
-          
+          header_buf[buf_count] = current;
+          ++count;
+          ++buf_count;
+          current = input[count];
         }
+        pair -> header = strdup(header_buf);
         break;
       case RIGHT_BRACKET:
         /* right bracket without a left bracket is an error */
@@ -42,6 +53,8 @@ struct ini* parse_line(char* input) {
         /* we just want to ignore whitespace */
         break;
       default:
+        /* by default, no behavior should happen */
+        /* It should break if a character is left unclosed or unopened */
         break;
     }
     ++count;
@@ -56,6 +69,8 @@ void error(char* input) {
 }
 
 int main(void) {
-  struct ini* ini = parse_line("\\n indicates a new line.\n");
+  struct ini* ini = parse_line("[Header]\n");
+  printf("The parsed header in this line is %s", ini -> header);
+  free(ini -> header);
   return 0;
 }
